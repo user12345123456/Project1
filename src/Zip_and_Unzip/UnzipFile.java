f5834e26173c408564ab54346cebc46ef3245ea1
h <angelinacom157@gmail.com>
2023-01-13 14:12:19 +0300

    1

---
+++ src/Zip_and_Unzip/UnzipFile.java
@@ -0,0 +1,20 @@
+package Zip_and_Unzip;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+
+public class UnzipFile {
+    public static File newFile(File destinationDir, ZipEntry zipEntry) throws IOException {
+        File destFile = new File(destinationDir, zipEntry.getName());
+
+        String destDirPath = destinationDir.getCanonicalPath();
+        String destFilePath = destFile.getCanonicalPath();
+
+        if (!destFilePath.startsWith(destDirPath + File.separator)) {
+            throw new IOException("Entry is outside of the target dir: " + zipEntry.getName());
+        }
+
+        return destFile;
+    }
+}
